58c23570d180b068c1436fe9496a09b1fc9f345d
Shakhawat Hosen <mdshakhawathosen122@gmail.com>
2024-03-26 14:24:46 +0600

    chore: minor changes

--- wpforminteg.php
+++ wpforminteg.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Integrations of Zoho Campaigns and CF7
- * Requires plugins: Contact Form 7
+ * Requires plugins: contact+form+7
  * Plugin URI:  https://formsintegrations.com/integration-of-zoho-campaigns-and-cf7
  * Description: This plugin integrates CF7 forms with Zoho Campaigns
  * Version:     1.0
